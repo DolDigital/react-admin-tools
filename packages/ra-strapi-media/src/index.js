c3aeeb69c1a3c5b6c300cf0a52322387e7eeace7
Luigi Pressello <l.pressello@dol.it>
2020-04-24 19:48:41 +0200

    Clean up old code, added filter on non existent files.

--- packages/ra-strapi-media/src/index.js
+++ packages/ra-strapi-media/src/index.js
@@ -138,169 +138,6 @@ const fixUploadUrl = media => {
   }
 }
 
-const MediaGallery = props => {
-  const {
-    options: { inputProps: inputPropsOptions, ...options } = {},
-    accept,
-    maxSize,
-    minSize,
-    multiple,
-    input,
-    closeMediaLibrary
-  } = props
-  const [pagination, setPagination] = useState((({ page = 1, perPage = 15 }) => ({ page, perPage }))(props))
-  const [sort, setSort] = useState((({ field = 'created_at', order = 'DESC' }) => ({ field, order }))(props))
-  const { data, total, ids, loading, error } = useGetList('upload/files', pagination, sort)
-  const [deleteDialog, setDeleteDialog] = useState(false)
-
-  const dataProvider = useDataProvider()
-
-  const selected = input.value ? (Array.isArray(input.value) ? input.value : [input.value]) : [];
-
-  const onSelect = media => {
-    if (!multiple) {
-      selected.push(media)
-      input.onChange(media)
-
-      return
-    }
-    if (selected.filter(item => item.id === media.id).length) {
-      input.onChange(selected.filter(item => item.id !== media.id))
-    } else {
-      input.onChange([...selected, media])
-    }
-  }
-
-  const handleDelete = media => {
-    setDeleteDialog(media)
-  }
-
-  const proceedWithDelete = media => {
-    dataProvider.delete('upload/files', { id: media.id }).then(r => {
-      if (!multiple) {
-        if (selected.filter(item => item.id === media.id).length === 1) {
-          input.onChange(null)
-        }
-      } else {
-        input.onChange(selected.filter(item => item.id !== media.id))
-      }
-      setDeleteDialog(false)
-      setPagination({ ...pagination, rnd: Math.random() })
-    })
-  }
-
-  const onDrop = (newFiles, rejectedFiles, event) => {
-    dataProvider._strapiUpload(newFiles).then(r => setPagination({ ...pagination, rnd: Math.random() }))
-  };
-
-  const { getRootProps, getInputProps } = useDropzone({
-    ...options,
-    accept,
-    maxSize,
-    minSize,
-    multiple,
-    onDrop,
-  });
-
-  if (loading) { return <div>Loading</div>; }
-  if (error) { return <p>ERROR</p>; }
-  return (
-    <>
-      {deleteDialog && <Dialog
-        open={deleteDialog !== false}
-        onClose={() => setDeleteDialog(false)}
-        aria-labelledby="draggable-dialog-title"
-      >
-        <DialogTitle id="draggable-dialog-title">
-          Delete media
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you shure you want to delete "{deleteDialog.name}"?<br />
-            The action is irreversible.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={() => setDeleteDialog(false)} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={() => proceedWithDelete(deleteDialog)} color="primary">
-            CONFIRM DELETION
-          </Button>
-        </DialogActions>
-      </Dialog>}
-      <GridList cellHeight={160} cols={5}>
-        {ids.map(key => {
-          const tile = fixUploadUrl(data[key])
-          return (
-            <GridListTile key={tile.id} cols={tile.cols || 1}>
-              {/image(.*)/.exec(tile.mime) ? <img src={tile.url} alt={tile.name} />
-                :
-                <DescriptionIcon />
-              }
-              <GridListTileBar
-                title={<>
-                  <Checkbox
-                    checked={selected.filter(item => item.id === tile.id).length === 1}
-                    onChange={() => onSelect(tile)}
-                    color="primary"
-                  />
-                  {tile.name}</>}
-                subtitle={<span>type: {tile.mime}, size: {tile.size} KB</span>}
-                actionIcon={
-                  <IconButton aria-label={`delete ${tile.name}`} onClick={() => handleDelete(tile)}>
-                    <DeleteForeverIcon color="secondary" />
-                  </IconButton>
-                }
-              />
-            </GridListTile>
-          )
-        })}
-      </GridList>
-      {total && <TablePagination
-        rowsPerPageOptions={[15]}
-        component="div"
-        count={total}
-        rowsPerPage={pagination.perPage}
-        page={pagination.page - 1 || 0}
-        onChangePage={(event, newPage) => setPagination({ ...pagination, page: newPage + 1 })}
-      />}
-      <div
-        data-testid="dropzone"
-        style={{ border: '3px dashed #ccc', padding: '2em', textAlign: 'center' }}
-        {...getRootProps()}
-      >
-        <input
-          {...getInputProps({
-            ...inputPropsOptions,
-          })}
-        />
-        Upload new files by clicking or dropping here.
-      </div>
-      <Button onClick={() => closeMediaLibrary()}>Close</Button>
-    </>
-  )
-}
-
-const MediaListItem = props => {
-  const { item, input } = props
-  const isSingle = !Array.isArray(input.value)
-  const handleRemove = () => {
-    if (isSingle) {
-      input.onChange(null)
-    } else {
-      input.onChange(input.value.filter(d => d.id !== item.id))
-    }
-  }
-  const media = fixUploadUrl(item)
-  return <ListItem button>
-    <ListItemIcon>
-      <HighlightOffIcon onClick={() => handleRemove()} />
-    </ListItemIcon>
-    <ListItemText primary={<>{media.name}{/image(.*)/.exec(media.mime) ? <img src={media.url} alt={media.name} style={{ width: '2em' }} /> : null}</>} />
-  </ListItem>
-}
-
 const LibraryComponent = props => {
   const [pagination, setPagination] = useState((({ page = 1, perPage = 10 }) => ({ page, perPage }))(props))
   const [sort, setSort] = useState((({ field = 'created_at', order = 'DESC' }) => ({ field, order }))(props))
@@ -545,7 +382,9 @@ const TabbedModalContent = props => {
   
   const { onClose, input, multiple = false, allowedTypes = null } = props
 
-  const _selected = (input.value !== false ? (Array.isArray(input.value) ? input.value : [input.value]) : []).map(f => fixUploadUrl(f));
+  const _selected = (input.value !== false ? (Array.isArray(input.value) ? input.value : [input.value]) : [])
+    .filter(f => typeof f.id !== 'undefined')
+    .map(f => fixUploadUrl(f));
 
   const [tab, setTab] = useState(0)
   const [search, setSearch] = useState('')
@@ -624,7 +463,7 @@ const TabbedModalContent = props => {
 
 const MediaList = props => {
   const { media = [], ...otherProps } = props
-  const items = Array.isArray(media) ? media : [media]
+  const items = (Array.isArray(media) ? media : [media]).filter(i => typeof i.id !== 'undefined')
   
   if(media === false) return null
 
